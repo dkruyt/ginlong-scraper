0e01643af6097c4602d287c23f8326468c7cee9d
Gentleman1983 (Christian Otto) <Christian@Otto-Uslar.de>
2023-06-02 20:14:11 +0200

    Removed unused lan parameter

--- ginlong_solis_api_connector.py
+++ ginlong_solis_api_connector.py
@@ -30,7 +30,6 @@ def do_work():  # pylint: disable=too-many-locals disable=too-many-statements
     domain = os.environ['SOLIS_CLOUD_API_URL']
     port = os.environ['SOLIS_CLOUD_API_PORT']
     url = f'{domain}:{port}'
-    # lan = os.environ['GINLONG_LANG']
     device_id = 0   # os.environ['SOLIS_CLOUD_API_INVERTER_ID']
 
     # == Constants ===============================================================
